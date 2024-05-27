d84b0c8eb77de1f981cfa27c0e95e1479a91d597
aparnaqa1998 <aparnasastry2021@gmail.com>
2024-05-26 21:10:37 -0600

    adding keyvault changes

--- server.js
+++ server.js
@@ -3,24 +3,26 @@ const sql = require('mssql');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const { getSecret } = require('./keyvault');
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'public')));
+async function initializeServer() {
+    try {
+const dbUsername = await getSecret('db-admin-user');
+const dbPassword =await getSecret('db-password');
+const dbName = await getSecret('db-name');
 const dbConfig = {
-    user: 'admin-webapp-dev',
-    password: 'Param@2025$#',
-    server: 'sql-db-users-dev.database.windows.net',
-    database: 'db-foodbank',
-    options: {
-        encrypt: true,
-        enableArithAbort: true
-    }
-};
+        user:  dbUsername,
+        password: dbPassword,
+        server: 'sql-db-users-dev.database.windows.net',  // Replace with your database server name
+        database: dbName,
 
-sql.connect(dbConfig, err => {
+};
+await sql.connect(dbConfig, err => {
     if (err) console.log(err);
     else console.log('Database connected');
-});
-app.use(express.static(path.join(__dirname, 'public')));
+}); 
+
 
 app.get('/foodbanks', (req, res) => {
     const location = req.query.location;
@@ -36,7 +38,13 @@ app.get('/foodbanks', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+    }catch (err) {
+        console.error('Error initializing server:', err);
+      }
+    }
+    
+    initializeServer();
